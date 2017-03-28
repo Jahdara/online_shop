ee4fb9daeaf284ba8a9fcf2aa66553416b2bb14d
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-03-28 11:37:16 +0100

    setting a pdo instance n verbose error mode

--- www/test.php
+++ www/test.php
@@ -1,10 +1,16 @@
-<?php
+<?php #test a sandbox
 
 	define('DBNAME','online_store');
 	define('DBUSER', 'root');
 	define('DBPASS', 'native');
 
 
+try{
+	#prepare a pdo instance
 	$con=new PDO('mysql:host=localhost;online_store=test'.DBNAME, DBUSER, DBPASS);
+
+	#set a verbose error mode
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+}
 
 ?>
