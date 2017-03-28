7f9c041faab08a229c0596ddbcf2e7610612e496
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-03-28 11:59:21 +0100

    define db connection constant

---
+++ www/includes/db.php
@@ -0,0 +1,18 @@
+<?php #define db connection constant...
+
+	define('DBNAME','online_store');
+	define('DBUSER', 'root');
+	define('DBPASS', 'native');
+
+
+try{
+	#prepare a pdo instance
+	$con=new PDO('mysql:host=localhost;online_store=test'.DBNAME, DBUSER, DBPASS);
+
+	#set a verbose error mode
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+} catch(PDOException $e) {
+	echo $e->getMessage();
+}
+
+?>
