a2b2fa480aa7194824cfb287540e89bb6a9b33e9
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-03-30 12:12:48 +0100

    insert data

--- www/register.php
+++ www/register.php
@@ -43,6 +43,9 @@
 
  			#hash the password
  			$hash = password_hash($clean['password'], PASSWORD_BCRYPT);
+
+ 			#insert data
+ 			$stmt = $con ->prepare("INSERT INTO admin(firstname, lastname, email, hash) VALUES(:fn, :ln, :e, :h)");
  		
  		}
 
