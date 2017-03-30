02a36d4c726dc8a9a507760238d4ee769795c4a4
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-03-30 16:35:00 +0100

    editing params function

--- www/includes/function.php
+++ www/includes/function.php
@@ -4,6 +4,19 @@
 		#hash the password
 		$hash = pasword_hash($input['password'], PASSWORD_BCRYPT);
 
+		#insert data
+		$stmt = $dbcon->prepare("INSERT INTO admin (firstname, lastname, email, hash) VALUES(:fn, :ln, :e, :h)");
+
+		#bind params...
+		$data = [
+			':fn' => $input['fname'],
+			':ln' => $input['lname'],
+			':e' => $input['email'],
+			':h' => $hash
+		];
+
+		$stmt -> execute($data);
+
 	}
 
 ?>
