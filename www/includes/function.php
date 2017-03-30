ace2fe4ac6bd1896881db88462cc108a8b243986
eluyefa oluwakayode <oeluyefa@gmail.com>
2017-03-30 17:04:02 +0100

    password error

--- www/includes/function.php
+++ www/includes/function.php
@@ -2,7 +2,7 @@
 
 	function doAdminRegister($dbcon, $input){
 		#hash the password
-		$hash = pasword_hash($input['password'], PASSWORD_BCRYPT);
+		$hash = password_hash($input['password'], PASSWORD_BCRYPT);
 
 		#insert data
 		$stmt = $dbcon->prepare("INSERT INTO admin (firstname, lastname, email, hash) VALUES(:fn, :ln, :e, :h)");
